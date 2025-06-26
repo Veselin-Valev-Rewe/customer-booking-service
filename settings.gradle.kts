6c4f349da186a423a0bced0b4c699c7c74db2972
Veselin-Valev-Rewe <veselin.valev@rewe-group.com>
2025-06-26 09:36:51 +0300

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "customer-booking-service"
---
+++ src/main/java/com/example/customer_booking_service/CustomerBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.customer_booking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerBookingServiceApplication.class, args);
+	}
+
+}
